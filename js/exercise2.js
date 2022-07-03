5fa30ac2ecccdde356c7f93343faedfd1ae1f85f
juanayala1 <juan.ayala@condorlabs.io>
2022-07-02 21:58:10 -0500

    create logic to solve the second exercise

--- js/exercise2.js
+++ js/exercise2.js
@@ -1,34 +1,89 @@
-
-    let array = [10,20,30,40];
-    // let previous;
-    let biggerNum;
-    let minorNum;
-    array.map((current, item, numb) => { 
-
-        if(item !== 0){
-
-          let previous = numb[item-1]
-          if(current === previous) {
-            alert('No puede ingresar valores iguales');
-          }
-
-          if(current > previous) {
-            biggerNum = current;
-          } else {
-            biggerNum = previous;
-          }
-
-          if(current < previous) {
-            minorNum = current;
-          } else {
-            minorNum = previous;
-          }
-
-        } else {
-            biggerNum = current;
-            minorNum = current;
-        }      
-    });
     
-    let mayor = biggerNum;
-    let menor = minorNum;
+    const checkIsNumber = (value) => {
+
+      var reg = new RegExp('^[0-9]*$');
+
+      if (reg.test(value)==false) {
+        alert(`${value} no es un número válido`);
+        throw new TypeError(`${value} no es un número válido`);
+      } 
+
+    }
+
+    const calculateNumbers = () => {
+
+      let valueA = getValueFromInput('numberA');
+      let valueB = getValueFromInput('numberB');
+      let valueC = getValueFromInput('numberC');
+      let valueD = getValueFromInput('numberD');
+
+      let arrayNumbers = [valueA, valueB, valueC, valueD];
+
+      checkDuplicateItemsIntoArray(arrayNumbers);
+
+      arrayNumbers.map((number) => {
+        checkIsNumber(number);
+      })
+      
+      // let previous;
+      let biggerNum;
+      let minorNum;
+
+      arrayNumbers.map((current, item, numb) => { 
+  
+          if(item !== 0) {
+  
+            let previous = numb[item-1]
+  
+            if(current > previous) 
+              biggerNum = current > biggerNum ? current : biggerNum;
+            else 
+              biggerNum = previous > biggerNum ? previous : biggerNum;
+            
+  
+            if(current < previous) 
+                minorNum = current < minorNum ? current : minorNum;
+            else 
+                minorNum = previous < minorNum ? previous : minorNum;
+            
+  
+          } else {
+              biggerNum = current;
+              minorNum = current;
+          }      
+      });
+      
+      let mayor = biggerNum;
+      let menor = minorNum;
+
+      alert('mayor: ' + mayor);
+      alert('menor: ' + menor);
+  
+    }
+
+    const getValueFromInput = (inputId) => {
+      let number = document.getElementById(inputId).value;
+
+      if(number === '') {
+        alert('Por favor complete todos los campos.');
+        throw new TypeError('Por favor complete todos los campos.');
+      }
+
+      number = !isNaN(number) ? Number(number) : number;
+        
+      return number;
+    }
+
+    const checkDuplicateItemsIntoArray = (array) => {
+
+      const tempArray = [...array].sort()
+
+      for (let i = 0; i < tempArray.length; i++) {
+        if (tempArray[i + 1] === tempArray[i]) {
+          alert('No puede ingresar valores iguales');
+          throw new TypeError('No puede ingresar valores iguales');
+        }
+      }
+
+    }
+
