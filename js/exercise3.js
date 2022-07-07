2b360c16348ca4217b3e08aa2caa15d01e99b92c
juanayala1 <juan.ayala@condorlabs.io>
2022-07-06 20:52:04 -0500

    ejercicio arreglos

---
+++ js/exercise3.js
@@ -0,0 +1,34 @@
+
+const functionParams =(stringParam) => {
+
+    let arrayString = stringParam.split(' ');
+
+    console.log('Array with string params: ' + JSON.stringify(arrayString, null, 2));
+    console.log('Count words ' + arrayString.length);
+    console.log('Firts word: ' + arrayString.shift());
+    console.log('Last word: ' + arrayString.pop());
+    arrayString = stringParam.split(' ');
+    // console.log('Reverse word: ' + arrayString.reverse());
+    console.log('Reverse word: ' + arrayString.reverse());
+
+    // console.log('From A to Z: ' + arrayString.sort());
+    // console.log('From Z to A: ' + descOrder(arrayString));
+
+}
+
+const descOrder = (array) => {
+   return array.sort(function (a, b) {
+        if (a > b) {
+            return -1;
+        }
+        if (b > a) {
+            return 1;
+        }
+        return 0;
+    });
+}
+
+(function(){
+    functionParams('Hola compañeros de diplomado frontend');
+})();
+
